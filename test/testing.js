ebb506a2ca7512986f2e8a2e49c4b1bea0b9770c
Anthony Orr <67woodcarver@gmail.com>
2022-11-24 20:13:59 -0500

    Refactored #bg

--- test/testing.js
+++ test/testing.js
@@ -2,18 +2,15 @@ const path = require('path')
 const fs = require('fs')
 const less = require("less")
 
+const testMe = function(lessInput) {
 
-const args = process.argv.slice(2);
-
-var lessInput = `.sticky(@len: 0; @side: bottom) {
-	position: sticky;
-	@{side}: @len;
-}`
-
-less.render(lessInput)
+	less.render(lesss)
     .then(function(output) {
-//        console.log(process.argv[2]);
-		console.log(output.css.toString())
-// output.css = string of css
-        // output.map = string of sourcemap
-})
+        console.log('output', output.css)
+	},
+	function (error) {
+		console.log('error', error)
+	})
+}
+
+export {testMe}
